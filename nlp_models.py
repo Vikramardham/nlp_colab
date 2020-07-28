aa2ba15ecf94bacb351f32f2daa9cf1db7a452ac
Vikram <Vikramardham@noreply.github.com>
2020-07-28 14:47:36 -0400

    'nlp text classification problem'

--- nlp_models.py
+++ nlp_models.py
@@ -1,180 +1,205 @@
-#models
+'''
+File Defining RNN Architectures and Custom Datasets for RNN and Transformer Models
+Vikram Reddy Ardham
+'''
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
-from torchviz import make_dot
-torch.manual_seed(1)
-import wandb
-# Custom Dataset
-class mydataset(Dataset):
-    
-    def __init__(self, x, y):
-        super(mydataset, self).__init__()
-        self.x = torch.tensor(x)
-        self.y = torch.tensor(y)
-        #self.x = transforms.ToTensor()(x)
-        #self.y = transforms.ToTensor()(y)
-    
-    def __len__(self):
-        return self.x.size()[0]
-        
-    def __getitem__(self, ix):
-        return self.x[ix], self.y[ix]
-    
-# PyTorch model
-class SimpleRNN(nn.Module):
-    
-    def __init__(self, emb_enc, seq_ln, emb_sz, hd_sz, vocab_sz):
-        super(SimpleRNN, self).__init__()
-        
-        self.output_sz = 5
-        #self.inp_sz = inp_sz
-        self.emb_sz = emb_sz
-        self.hd_sz = hd_sz
-        
-        #self.emb = nn.Embedding(vocab_sz, emb_sz)
-        self.emb = emb_enc
-        #self.dropout = nn.Dropout(p=dp)
-        self.i2h = nn.Linear(emb_sz, self.hd_sz)
-        self.h2h = nn.Linear(hd_sz, hd_sz)
-        self.h = torch.zeros(1, self.hd_sz)
-        self.h2o = nn.Linear(hd_sz*seq_ln, 5)
-        self.bn = nn.BatchNorm1d(5)
-        self.dp = nn.Dropout(p=0.2)
-    def forward(self, x):
-        emb = self.emb(x)
-        h = self.h
-        out = []
 
-        for xi in torch.transpose(emb, 0, 1):
-            i2h = self.i2h(xi)
-            h2h = torch.tanh(i2h + self.h2h(h))
-            out.append(h2h)
-        y = torch.flatten(torch.stack(out, dim=1), start_dim=1)
-        y = F.log_softmax(self.bn(self.h2o(y)), dim=1)
-        return y
-#GRU 
+class mydataset(Dataset):
+  '''
+  Pytorch Dataset class to create a a dataloader
+  '''
+  def __init__(self, x, y):
+    super().__init__()
+    self.x = torch.tensor(x, dtype=torch.long)
+    self.y = torch.tensor(y)
+  
+  def __len__(self):
+    return self.x.size()[0]
+      
+  def __getitem__(self, ix):
+    return self.x[ix], self.y[ix]
+
+class CustomDataset(Dataset):
+  '''
+  Custom Dataset that tokenizes input text data for the transformer model
+  '''
+  def __init__(self, x, y, tokenizer, max_len):
+    self.tokenizer = tokenizer
+    self.data = x
+    self.targets = y
+    self.maxlen = max_len
+  
+  def __len__(self):
+    return len(self.data)
+
+  def __getitem__(self, idx):
+    inputs = self.tokenizer.encode_plus(self.data[idx], add_special_tokens=True, 
+                                        max_length=self.maxlen, pad_to_max_length=True,
+                                        return_token_type_ids=True, truncation=True)
+    return {'ids': torch.tensor(inputs['input_ids'], dtype=torch.long),
+            'mask':torch.tensor(inputs['attention_mask'], dtype=torch.long),
+            'token_type_ids':torch.tensor(inputs['token_type_ids'], dtype=torch.long),
+            'targets':torch.tensor(self.targets[idx], dtype=torch.long)}
+
+class SpatialDropout(nn.Dropout2d):
+  '''
+  Spatial Dropout for the embedding layer
+  Refactor the existing Dropou2d class in PyTorch
+  '''
+  def forward(self, inps):
+    inps =inps.unsqueeze(2)
+    inps = inps.permute(0, 3, 2, 1)
+    inps = super().forward(inps)
+    inps = inps.permute(0, 3, 2, 1)
+    inps = inps.squeeze(2)
+    return inps
+
+class myArch(nn.Module):
+  '''Basic RNN based Text-classification architecture'''
+  def __init__(self, emb_enc, seq_ln, hd_sz, stacks=2, CHOICE='LSTM'):
+    super().__init__()
+    self.emb = emb_enc
+    self.seq_ln = seq_ln
+    self.hd_sz = hd_sz
+    self.emb_sz = emb_enc.embedding_dim
+    self.bidirectional = False
+    self.emb_dropout = SpatialDropout(p=0.4)
+    
+    # Choice of the type of RNN
+    ARCH = {'LSTM': nn.LSTM(input_size=self.emb_sz, 
+                          hidden_size=self.hd_sz,
+                          num_layers =stacks, bias=True, 
+                          bidirectional=self.bidirectional, dropout=0.8,
+                          batch_first=True), 
+              'GRU': SimpleGRU(self.seq_ln, self.emb_sz, self.hd_sz) , 
+              'Vanilla RNN': SimpleRNN(self.seq_ln, self.emb_sz, self.hd_sz), }
+      
+    self.RNN = ARCH[CHOICE]
+    
+    self.dropout = nn.Dropout(p=0.4)
+    Nout = 128
+    self.linear = nn.Linear(self.hd_sz*2*(1+int(self.bidirectional)), Nout)
+    self.batchnorm = nn.BatchNorm1d(Nout)
+    self.out = nn.Linear(Nout, 5)
+
+  def forward(self, X):
+    '''
+    1. Embedding layer
+    2. Spatial Dropout
+    3. RNN layer
+    4. concatenate (avg_pool, max_pool)
+    5. Linear Layer
+    5. Batch Normalization
+    6. Dropout
+    7. Linear Layer to output class probabilities (5 classes here)
+    '''
+    h = self.emb(X)
+    h = self.emb_dropout(h)
+    gru_out, _ = self.RNN(h)
+
+    avg_pool = torch.mean(gru_out, 1)
+    max_pool, _ = torch.max(gru_out, 1)
+    conc = torch.cat((avg_pool, max_pool), 1)
+    
+    conc = self.batchnorm(nn.ReLU()(self.linear(conc)))
+    conc = self.dropout(conc)
+    out = self.out(conc)
+    
+    return out
+
+# My RNN
+class SimpleRNN(nn.Module):
+  '''Vanilla RNN implemented from Scracth '''
+  def __init__(self, seq_ln, emb_sz, hd_sz):
+    super(SimpleRNN, self).__init__()
+    
+    self.output_sz = 5
+    self.hd_sz = hd_sz
+    
+    self.h = torch.zeros(1, self.hd_sz)
+    self.i2h = nn.Linear(emb_sz, self.hd_sz)
+    self.h2h = nn.Linear(hd_sz, hd_sz)
+  
+  def forward(self, x):
+    '''
+    1. Input (embedding) to hidden
+    2. Hidden to hidden
+    3. Add them and apply activation
+    '''
+    h = self.h
+    out = []
+    for xi in torch.transpose(x, 0, 1):
+        i2h = self.i2h(xi)
+        h2h = torch.tanh(i2h + self.h2h(h))
+        out.append(h2h)
+    return torch.stack(out, dim=1), 1 # Dummpy output as tuple to have a consistent structure with PyTorch
+
+#rnn_loop
 def rnn_loop(cell, h, x):
-    res = []
-    for x_ in x.transpose(0,1):
-        #print('x_', x_.size())
-        h = cell(x_, h)
-        res.append(h)
-    return torch.stack(res, dim=1)
+  res = []
+  for x_ in x.transpose(0,1):
+      h = cell(x_, h)
+      res.append(h)
+  return torch.stack(res, dim=1)
+
 
 class GRUCell(nn.Module):
-    def __init__(self, ni, nh):
-        super(GRUCell, self).__init__()
-        self.ni, self.nh = ni, nh
-        self.i2h = nn.Linear(ni, 3*nh)
-        self.h2h = nn.Linear(nh, 3*nh)
-    
-    def forward(self, x, h):
-        gate_x = self.i2h(x).squeeze()
-        gate_h = self.h2h(h).squeeze()
-        i_r,i_u,i_n = gate_x.chunk(3, 1)
-        h_r,h_u,h_n = gate_h.chunk(3, 1)
-        
-        #print(gate_x.size(), gate_h.size(), i_r.size(), h_r.size(), x.size())
-        resetgate = torch.sigmoid(i_r + h_r)
-        updategate = torch.sigmoid(i_u + h_u)
-        newgate = torch.tanh(i_n + (resetgate*h_n))
-        return updategate*h + (1-updategate)*newgate
-    
+  '''Inspired from the fastai-nlp course'''
+  def __init__(self, ni, nh):
+    super(GRUCell, self).__init__()
+    self.ni, self.nh = ni, nh
+    self.i2h = nn.Linear(ni, 3*nh)
+    self.h2h = nn.Linear(nh, 3*nh)
+  
+  def forward(self, x, h):
+    '''
+    return a weighted mean of old and newgates
+    weight is the udpategate
+    '''
+    gate_x = self.i2h(x).squeeze()
+    gate_h = self.h2h(h).squeeze()
+    i_r,i_u,i_n = gate_x.chunk(3, 1)
+    h_r,h_u,h_n = gate_h.chunk(3, 1)
+  
+    resetgate = torch.sigmoid(i_r + h_r)
+    updategate = torch.sigmoid(i_u + h_u)
+    newgate = torch.tanh(i_n + (resetgate*h_n))
+    return updategate*h + (1-updategate)*newgate
+
+#My GRU   
 class SimpleGRU(nn.Module):
-    def __init__(self, emb_enc, seq_ln, emb_sz, hd_sz, vocab_sz, bs= 64):
-        super(SimpleGRU, self).__init__()
-        #self.i_h = nn.Embedding(vocab_sz, emb_sz)
-        self.i_h = emb_enc
-        self.h_o = nn.Linear(hd_sz*seq_ln, 5)
-        self.bn = nn.BatchNorm1d(5)
-        self.hd_sz = hd_sz
-        self.rnnc = GRUCell(emb_sz, hd_sz)
-        
-    def forward(self, x):
-        #print(emb_sz)
-        #print('input:', self.i_h(x).size())
-        #self.h = torch.zeros(1, )
-        i_h = self.i_h(x)
-        h = torch.zeros(1, i_h.size()[0], self.hd_sz)
-        res = rnn_loop(self.rnnc, h, i_h)
-        self.h = res[:, -1].detach()
-        res = torch.transpose(torch.squeeze(res), 0, 1)
-        out = self.bn(self.h_o(torch.flatten(res, start_dim=1)))
-        return F.log_softmax(out, dim=1)
+  '''
+  Combine the GRUcell with the RNN loop
+  '''
+  def __init__(self, seq_ln, emb_sz, hd_sz):
+    super().__init__()
+    self.hd_sz = hd_sz
+    self.rnnc = GRUCell(emb_sz, hd_sz)
+      
+  def forward(self, x):
+    h = torch.zeros(1, x.size()[0], self.hd_sz)
+    res = rnn_loop(self.rnnc, h, x)
+    self.h = res[:, -1].detach()
+    res = torch.transpose(torch.squeeze(res), 0, 1)
+    return res, 1 # Dummy output 1 
 
-class TorchRNN(nn.Module):
-    def __init__(self, emb_sz, inp_sz, vocab_sz, hd_sz, out_sz):
-        super(TorchRNN, self).__init__()
-        self.inp_sz = inp_sz
-        self.hd_sz = hd_sz
-        
-        self.rnn = nn.Sequential(nn.Embedding(vocab_sz, emb_sz),
-                          nn.RNN(input_size=emb_sz, hidden_size=hd_sz, batch_first=True))
-        self.out = nn.Linear(inp_sz* hd_sz, out_sz)
-        self.activ = nn.LogSoftmax(dim=1)
-        
-    def forward(self, x):
-        rnn_out = self.rnn(x)
-        #print(rnn_out[0].reshape(-1, inp_sz*hd_sz).size())
-        final_out = self.activ(self.out(rnn_out[0].reshape(-1, self.inp_sz*self.hd_sz)))
-        #print(final_out.size())
-        return final_out
+class BertModel(nn.Module):
+  '''Bert Model Class for text-classification 
+  Adding a dropout layer and linear layer to spit out 5 class probabilities'''
+  def __init__(self):
+    super().__init__()
+    self.model_name = 'bert-base-cased'
+    self.l1 = transformers.BertModel.from_pretrained(self.model_name)
+    self.l2 = nn.Dropout(0.3)
+    self.l3 = nn.Linear(768, 5)
 
-class TorchGRU(nn.Module):
-    def __init__(self, emb_sz, inp_sz, vocab_sz, hd_sz, out_sz):
-        super(TorchRNN, self).__init__()
-        self.inp_sz=inp_sz
-        self.hd_sz = hd_sz
-        self.rnn = nn.Sequential(nn.Embedding(vocab_sz, emb_sz),
-                          nn.RNN(input_size=emb_sz, hidden_size=hd_sz, batch_first=True))
-        self.out = nn.Linear(inp_sz* hd_sz, out_sz)
-        self.activ = nn.LogSoftmax(dim=1)
-        
-    def forward(self, x):
-        rnn_out = self.rnn(x)
-        #print(rnn_out[0].reshape(-1, inp_sz*hd_sz).size())
-        final_out = self.activ(self.out(rnn_out[0].reshape(-1, self.inp_sz*self.hd_sz)))
-        #print(final_out.size())
-        return final_out
-#class SimpleGRU(nn.Module):
-#    
-#    def __init__(self, inp_sz, emb_sz, hd_sz, output_sz, vocab_sz):
-#        super(SimpleGRU, self).__init__()
-#        
-#        self.output_sz = output_sz
-#        self.inp_sz = inp_sz
-#        self.emb_sz = emb_sz
-#        self.hd_sz = hd_sz
-        
-#        self.emb = nn.Embedding(vocab_sz, emb_sz)
-#        self.dropout = nn.Dropout(p=dp)
-        
-#        self.update = nn.Linear(self.emb_sz+self.hd_sz, self.hd_sz)
-#        self.reset = nn.Linear(self.emb_sz+self.hd_sz, self.hd_sz)
-#        self.output = nn.Linear(self.emb_sz+self.hd_sz, self.hd_sz)
-#        self.final = nn.Linear(self.inp_sz*self.hd_sz, self.output_sz)
-#        self.h = torch.zeros(1, hd_sz)
-#        self.activ = nn.LogSoftmax(dim=1)
-                             
-#    def forward(self, x):
-        
-#        emb = self.dropout(self.emb(x))
-#        h = self.h.view(1, -1).repeat(x.size()[0], 1)
-#        out=[]
-        
-#        for xi in torch.transpose(emb, 0, 1):
-#            combined = torch.cat( (xi, h), dim=1)
-#            zt = torch.sigmoid(self.update(combined))
-#            reset = torch.sigmoid(self.reset(combined))
-#            output = torch.tanh(self.output(combined))
-            
-#            h = zt*h + (1-zt)*output
-#            out.append(output)
-            
-#        y = torch.cat(out).view(-1, self.inp_sz*self.hd_sz)
-        
-        #print(y.size())
-#        return self.activ(self.final(y))
+  def forward(self, ids, mask, token_type_ids):
+    output_1 = self.l1(ids, attention_mask = mask, token_type_ids=token_type_ids)
+    output_2 = self.l2(output_1[1])
+    output = self.l3(output_2)
+    return output
+    
